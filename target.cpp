d147daacad153f84f644a868e3f5f6caa31bcafc
glafratta <g.lafratta@outlook.com~>
2025-02-07 17:21:43 +0000

    coded set target

--- target.cpp
+++ target.cpp
@@ -62,8 +62,15 @@ void forget(Configurator *c){}
 
 
 Disturbance set_target(int& run, b2Transform start){
-	return Disturbance(PURSUE, b2Vec2(1.0f, 0.0f), 0.0f);
-
+	Disturbance result;
+	if (run%2!=0 && run >0){
+		result= Disturbance(PURSUE, b2Vec2(1.0f, 0.0f), 0.0f);
+	}
+	else{
+		result= Disturbance(PURSUE, b2Vec2(-1.0f, 0.0f), 0.0f);
+	}
+	run++;
+	return result;
 }
 
 int main(int argc, char** argv) {
